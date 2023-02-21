280c161cfc012f151779f3e97e8ee16f94d9e271
Niimaii <nimaicgomes@gmail.com>
2023-02-21 18:21:57 -0300

    worked on register function

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,4 +1,5 @@
 const db = require("../db/index");
+const { hash } = require("bcryptjs");
 
 exports.getUsers = async (req, res) => {
   try {
@@ -10,9 +11,26 @@ exports.getUsers = async (req, res) => {
 };
 
 exports.register = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
+    // Inserting the incrypted password and standard email into db
+    const hashedPassword = await hash(password, 10);
+    await db.query("INSERT INTO users(email, password) VALUES ($1, $2)", [
+      email,
+      hashedPassword,
+    ]);
+
+    return res.status(201).json({
+      sucess: true,
+      message: "The registration was successful",
+    });
+
     console.log("validation passed");
   } catch (error) {
     console.log(error.message);
+    return res.status(500).json({
+      error: error.message,
+    });
   }
 };
