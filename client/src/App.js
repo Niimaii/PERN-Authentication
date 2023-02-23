ac80ae4f65c02dd345ac42703d6e3a46e769c451
Niimaii <nimaicgomes@gmail.com>
2023-02-22 23:00:17 -0300

    Private Routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,33 @@ import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
+const PrivateRoutes = () => {
+  const isAuth = false;
+
+  // If the user is authenticated render the content for the page, else got to the login page.
+  return <>{isAuth ? <Outlet /> : <Navigate to="/login" />}</>;
+};
+
+const RestrictedRoutes = () => {
+  const isAuth = false;
+
+  // If the user is not authenticated render the content for the page (namely register & login), else got to the dashboard page.
+  return <>{!isAuth ? <Outlet /> : <Navigate to="/dashboard" />}</>;
+};
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+
+        <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<RestrictedRoutes />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
