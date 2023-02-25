5b130b577193c59ed22977022fccdfc00e6bab91
Niimaii <nimaicgomes@gmail.com>
2023-02-24 23:46:27 -0300

    Login Added

--- client/src/App.js
+++ client/src/App.js
@@ -4,39 +4,40 @@ import {
   Routes,
   Route,
   Outlet,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import { useSelector } from 'react-redux';
 
 const PrivateRoutes = () => {
-  const isAuth = false;
+  const { isAuth } = useSelector((state) => state.auth);
 
   // If the user is authenticated render the content for the page, else got to the login page.
-  return <>{isAuth ? <Outlet /> : <Navigate to="/login" />}</>;
+  return <>{isAuth ? <Outlet /> : <Navigate to='/login' />}</>;
 };
 
 const RestrictedRoutes = () => {
-  const isAuth = false;
+  const { isAuth } = useSelector((state) => state.auth);
 
   // If the user is not authenticated render the content for the page (namely register & login), else got to the dashboard page.
-  return <>{!isAuth ? <Outlet /> : <Navigate to="/dashboard" />}</>;
+  return <>{!isAuth ? <Outlet /> : <Navigate to='/dashboard' />}</>;
 };
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
 
         <Route element={<PrivateRoutes />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Route>
         <Route element={<RestrictedRoutes />}>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,32 @@
 // import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-  const isAuth = false;
+  const { isAuth } = useSelector((state) => state.auth);
 
   return (
-    <nav className="main-nav">
-      <div className="nav-container">
-        <div className="nav-home-container">
-          <NavLink to="/">
-            <span className="nav-home">Home</span>
+    <nav className='main-nav'>
+      <div className='nav-container'>
+        <div className='nav-home-container'>
+          <NavLink to='/'>
+            <span className='nav-home'>Home</span>
           </NavLink>
         </div>
 
         {isAuth ? (
-          <div className="nav-content">
-            <NavLink to="/dashboard" className="mx-3">
+          <div className='nav-content'>
+            <NavLink to='/dashboard' className='mx-3'>
               <span>Dashboard</span>
             </NavLink>
           </div>
         ) : (
-          <div className="nav-content">
-            <NavLink to="/login">
+          <div className='nav-content'>
+            <NavLink to='/login'>
               <span>Login</span>
             </NavLink>
 
-            <NavLink to="/register" className=" mx-3">
+            <NavLink to='/register' className=' mx-3'>
               <span>Register</span>
             </NavLink>
           </div>
